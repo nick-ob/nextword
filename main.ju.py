0970dd345b0e83172a4fb457bc4cc4e0e8f6a625
nick-ob <obriennick09@gmail.com>
2026-05-04 18:49:28 +0200

    Added main entrypoint file

--- main.ju.py
+++ main.ju.py
@@ -1,1 +1,18 @@
+"""Main entrypoint file.
+See also notebooks/main.ipynb for a jupyter notebook version.
+"""
+# %% [markdown]
+# # NextWord
 
+# %% [markdown]
+# ## Loading input data
+
+# %%
+from src.loader import load_input
+from src.formatter import tokens_to_data
+
+# %%
+tokens_all, tokens_single = load_input("mini_test")
+(x_train, y_train), (x_test, y_test) = tokens_to_data(tokens_all, tokens_single)
+print(f"Training shapes: {x_train.shape} & {y_train.shape}")
+print(f"Testing shapes: {x_test.shape} & {y_test.shape}")
