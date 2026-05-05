44159311fa4ab197224df37b901beee226f0d8ad
nick-ob <obriennick09@gmail.com>
2026-05-05 13:16:02 +0200

    Updated main entrypoint file with examples

--- main.ju.py
+++ main.ju.py
@@ -12,7 +12,29 @@ from src.loader import load_input
 from src.formatter import tokens_to_data
 
 # %%
-tokens_all, tokens_single = load_input("mini_test")
+data: str = "mini_test.txt"
+tokens_all, tokens_single = load_input(data)
 (x_train, y_train), (x_test, y_test) = tokens_to_data(tokens_all, tokens_single)
+print(f"Loaded data from {data}:")
 print(f"Training shapes: {x_train.shape} & {y_train.shape}")
 print(f"Testing shapes: {x_test.shape} & {y_test.shape}")
+
+# %% [markdown]
+# ## Initialise a network
+
+# %%
+from src.model import Network
+
+# %%
+# either create
+network = Network(x_train.shape[1], 100, x_train.shape[1])
+
+# or load
+# network, histoy = Network.load("your_network")
+
+# %% [markdown]
+# ## Train the network
+
+# %%
+# skip/comment out if loading an existing network
+history = network.train((x_train, y_train), 0.01, 1000, batch_size=16)
