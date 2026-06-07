d89815cab19e61e8931a48d39d190c8127530441
nick-ob <188435909+nick-ob@users.noreply.github.com>
2026-06-07 12:57:12 +0200

    Updated main with first next word prediction

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ See also notebooks/main.ipynb for a jupyter notebook version.
 # %% [markdown]
 # # NextWord
 
+# %%
+import numpy as np
+
+from src.formatter import data_to_top_tokens, token_to_data, tokens_to_data
+from src.loader import load_input
+from src.model import Network
+
 # %% [markdown]
 # ## Loading input data
-
-# %%
-from src.loader import load_input
-from src.formatter import tokens_to_data
 
 # %%
 data: str = "mini_test.txt"
@@ -21,9 +24,6 @@ print(f"Testing shapes: {x_test.shape} & {y_test.shape}")
 
 # %% [markdown]
 # ## Initialise a network
-
-# %%
-from src.model import Network
 
 # %%
 # either create
@@ -38,3 +38,27 @@ network = Network(x_train.shape[1], 100, x_train.shape[1])
 # %%
 # skip/comment out if loading an existing network
 history = network.train((x_train, y_train), 0.01, 1000, batch_size=16)
+
+# %% [markdown]
+# ## Test the network
+
+# %%
+pred, y, acc = network.test((x_test, y_test))
+print(f"Testing accuracy: {acc}%")
+
+# %% [markdown]
+# ## Top next word predictions
+
+# %%
+while True:
+    user_input = input("Enter a word: ")
+    user_input = user_input.encode().decode("unicode_escape")
+    token = token_to_data(user_input, tokens_single)
+    if token is None:
+        print("Word not found in tokens")
+        break
+    else:
+        pred = network.predict(np.array([token]))
+        top = data_to_top_tokens(pred, tokens_single)
+        for token, prob in top.items():
+            print(f"{token!r}: {prob}%")
